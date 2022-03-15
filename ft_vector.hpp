b64fdb6bbacbf70cf9333d54d772543ee2918552
Clement BEAURAIN <cbeaurai@e2r3p4.clusters.42paris.fr>
2022-03-15 13:16:09 +0100

    save

--- ft_vector.hpp
+++ ft_vector.hpp
@@ -3,12 +3,23 @@
 
 # include <memory>
 
+/*https://en.cppreference.com/w/cpp/container/vector*/
+
 namespace ft {
 template <class T, class Allocator = std::allocator<T> >
 class vector
 {
 public:
     void disp() { std::cout <<"Lol" << std::endl;};
+/*==Member types==*/
+
+	typedef	T 					value_type;
+	typedef	Allocator			allocator_type;
+	typedef	std::size_t			size_type;
+	typedef	std::ptrdiff_t		difference_type;
+	typedef	value_type&			reference;
+	typedef	const value_type&	const_reference;
+	typedef	typename Allocator::pointer	pointer;
 /*==Basics==*/
 
     /*      Constructor     */
@@ -33,11 +44,38 @@ public:
     /*      Rend            */
 
 /*==Capacity==*/
+
     /*      Empty           */
     /*      Size            */
     /*      Max_size        */
     /*      Reserve         */
     /*      Capacity        */
+
+/*==Modifiers==*/
+
+	/*		Clear			*/
+	/*		Insert			*/
+	/*		Erase			*/
+	/*		Push_back		*/
+	/*		Pop-back		*/
+	/*		Resize			*/
+	/*		Swap			*/
 }; }
+
+/*==Operators overload==*/
+
+	/*		==				*/
+	/*		Const==			*/
+	/*		!=				*/
+	/*		<				*/
+	/*		<=				*/
+	/*		>=				*/
+	/*		>=				*/
+
+
+/*==Swap==*/
+
+	/*		Swap			*/
+
 
 #endif

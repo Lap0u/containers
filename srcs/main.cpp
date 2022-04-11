d502882f9ee4365f2e93be50ccd1ed0750950a66
Clement BEAURAIN <cbeaurai@e3r5p5.clusters.42paris.fr>
2022-04-11 17:39:13 +0200

    incre et decre a faire

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,22 +3,20 @@
 #define ENDL << std::endl
 #define NL COUT std::endl;
 
+#include <utility>
 #include <map>
 #include "../map.hpp"
 #include "../headers/RBT.hpp"
 
 int main()
 {
-	using namespace ft;
+	using namespace std;
 
 	map<char,int>re;
-	// re['e'] = 22;
-	re.add('e', 22);
+	pair<char,int>pp('r', 10);
+	re['e'] = 22;
+	// re.add('e', 22);
 	map<char,int>::iterator ite = re.begin();
-	map<char,int>::iterator ite2 = re.begin();
-	COUT ite->first ENDL;
-	COUT ite->second ENDL;
-	COUT (ite == ite2) ENDL;
-	COUT (ite != ite2) ENDL;
+	*ite = 21;
 
 }
